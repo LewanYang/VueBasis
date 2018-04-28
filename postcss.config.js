18274606a585da60da2b207281be66564321fd9a
lewanyang <3278353995@qq.com>
2018-04-28 15:52:47 +0800

    配置文件

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2018/4/8 0008.
+ */
+module.exports = {
+    plugins: [
+        require("autoprefixer")()
+    ]
+}
+
+// 在postcss-loader 加载的时候，就会自动读取配置文档中的配置项，加载autoprefixer
+
+
