25627710d618398c863c22e7c6ddbdf4613a8e95
lewanyang <3278353995@qq.com>
2018-04-30 20:53:13 +0800

    关于class绑定

---
+++ src/components/classbind.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2 @click="changeColor" :class="[isActived]">Beckham</h2>
+        <div v-if="show">拜仁慕尼黑</div>
+        <div v-else>德国队</div>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                isActived: '',
+                show: false
+            }
+        },
+        methods: {
+            changeColor: function () {
+                this.isActived = this.isActived === '' ? 'colorRed' : ''
+            }
+        }
+    }
+</script>
+<style type="text/css">
+    .colorRed {
+        color: red;
+    }
+</style>
