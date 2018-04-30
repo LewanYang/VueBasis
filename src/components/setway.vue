063f732a8d6e42b82307b20286010558cdd84780
lewanyang <3278353995@qq.com>
2018-04-30 20:53:57 +0800

    通过this.$set()来修改数组或对象

---
+++ src/components/setway.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <span>关于set方法在数组和对象中的使用</span>
+        <ul>
+            <li v-for="item in arr">{{item}}</li>
+        </ul>
+        <button @click="changeArr">改变数据</button>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                arr:[
+                    'beckham',
+                    'airJordan',
+                    'test',
+                    'muller',
+                    'gordan'
+                ]
+            }
+        },
+        methods: {
+            changeArr: function(){
+                this.$set(this.arr,1,'honda')
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
