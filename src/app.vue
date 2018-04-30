6fb1fe8754b75bb516c9020a591d3f4cfa80e64c
lewanyang <3278353995@qq.com>
2018-05-01 00:50:14 +0800

    app.vue

--- src/app.vue
+++ src/app.vue
@@ -2,12 +2,17 @@
     <div id="app">
         <input type="text" v-model="inputValue">
         <button @click="addItem">提交</button>
-        <ul>
-            <li v-for="item in list">{{item}}</li>
-        </ul>
+        <todo-item :list="list" @delete="deleteItem"></todo-item>
+        <methods></methods>
+        <class-bind></class-bind>
+        <set-way></set-way>
     </div>
 </template>
 <script>
+    import TodoItem from './components/todoitem.vue'
+    import Methods from './components/methods.vue'
+    import ClassBind from './components/classbind.vue'
+    import SetWay from './components/setway.vue'
     export default {
         data(){
             return {
@@ -19,7 +24,17 @@
             addItem:function(){
                 this.list.push(this.inputValue)
                 this.inputValue=''
+            },
+            deleteItem:function(index){
+                //alert(index)
+                this.list.splice(index,1)
             }
+        },
+        components: {
+            TodoItem,
+            Methods,
+            ClassBind,
+            SetWay
         }
     }
 </script>
