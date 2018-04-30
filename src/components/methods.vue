feb1eb733d0c5c28760444f73be462acd89b2ae7
lewanyang <3278353995@qq.com>
2018-04-30 14:19:38 +0800

    添加计算属性练习

---
+++ src/components/methods.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>{{fullName}}</h2>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                firstName: 'jay',
+                lastName: 'chou'
+            }
+        },
+        computed: {
+            fullName(){
+                return this.firstName + " " + this.lastName
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
