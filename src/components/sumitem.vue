2c18f05767552971cf1f505166404bfa9479ff7c
lewanyang <3278353995@qq.com>
2018-05-02 16:26:10 +0800

    关于ref在组件的使用

---
+++ src/components/sumitem.vue
@@ -0,0 +1,21 @@
+<template>
+    <div @click="handleClick">{{number}}</div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                number: 0
+            }
+        },
+        methods: {
+            handleClick:function(){
+                this.number++
+                this.$emit('changenum')
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
