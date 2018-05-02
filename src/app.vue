543dc20aecae5ad3a93e1b6c5b6fd2ff19652a45
lewanyang <3278353995@qq.com>
2018-05-02 16:36:39 +0800

    app.vue

--- src/app.vue
+++ src/app.vue
@@ -6,6 +6,10 @@
         <methods></methods>
         <class-bind></class-bind>
         <set-way></set-way>
+        <table-file></table-file>
+        <sum-item ref="one"  @changenum="sumNumber"></sum-item>
+        <sum-item ref="two" @changenum="sumNumber"></sum-item>
+        <div>{{sum}}</div>
     </div>
 </template>
 <script>
@@ -13,11 +17,14 @@
     import Methods from './components/methods.vue'
     import ClassBind from './components/classbind.vue'
     import SetWay from './components/setway.vue'
+    import TableFile from './components/table.vue'
+    import SumItem from './components/sumitem.vue'
     export default {
         data(){
             return {
                 inputValue: '',
-                list: []
+                list: [],
+                sum: 0
             }
         },
         methods: {
@@ -28,13 +35,20 @@
             deleteItem:function(index){
                 //alert(index)
                 this.list.splice(index,1)
+            },
+            sumNumber:function(){
+                console.log(this.$refs.one.number)
+                console.log(this.$refs.two.number)
+                this.sum=this.$refs.one.number+this.$refs.two.number
             }
         },
         components: {
             TodoItem,
             Methods,
             ClassBind,
-            SetWay
+            SetWay,
+            TableFile,
+            SumItem
         }
     }
 </script>
