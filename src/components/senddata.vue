466016ed0daaeba4600501477165efbb19de384d
lewanyang <3278353995@qq.com>
2018-05-02 17:23:50 +0800

    父子组件间的数据传递

---
+++ src/components/senddata.vue
@@ -0,0 +1,26 @@
+<template>
+    <div @click="handleClick">{{currentNumber}}</div>
+</template>
+<script>
+    export default {
+        props: {
+            count: {
+                type: Number,
+                required: true
+            }
+        },
+        data() {
+            return {
+                currentNumber: this.count
+            }
+        },
+        methods: {
+            handleClick:function(){
+                this.currentNumber++
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
