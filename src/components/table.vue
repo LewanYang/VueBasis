fc4e7fb3542fb25aaaa2228ca4bff9cc3c4ba50b
lewanyang <3278353995@qq.com>
2018-05-02 16:26:46 +0800

    table.vue

---
+++ src/components/table.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div ref="open" @click="handleClick">bayern</div>
+    </div>
+</template>
+<script>
+    export default {
+        methods: {
+            handleClick: function(){
+                console.log(this.$refs.open.innerHTML)
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
