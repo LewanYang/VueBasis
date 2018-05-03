22b62978e1a228f29e4268082e210df564d73fa1
lewanyang <3278353995@qq.com>
2018-05-03 16:37:11 +0800

    carton.vue

---
+++ src/components/carton.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <transition name="beauty">
+            <div v-if="show">hello world</div>
+        </transition>
+        <button @click="handleClick">改变</button>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                show: true
+            }
+        },
+        methods: {
+            handleClick: function () {
+                this.show = !this.show
+            }
+        }
+    }
+</script>
+<style type="text/css">
+    .beauty-enter {
+        opacity: 0;
+    }
+
+    .beauty-enter-active {
+        transition: opacity 2s
+    }
+
+    .beauty-leaves-to {
+        opacity: 0;
+    }
+
+    .beauty-leave-active {
+        transition: opacity 2s
+    }
+</style>
