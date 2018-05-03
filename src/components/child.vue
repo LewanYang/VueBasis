10ae9f79ca51d40a8f56b6653035b5bf82bbe237
lewanyang <3278353995@qq.com>
2018-05-03 14:56:10 +0800

    插槽的使用

---
+++ src/components/child.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <slot v-for="item in list" :item="item"></slot>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                list:['beckham','jay','kfc']
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
