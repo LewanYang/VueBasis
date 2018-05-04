4ae36b2379b080b21e2ad728c4c18bb89d625fa4
lewanyang <3278353995@qq.com>
2018-05-04 12:46:34 +0800

    vue中使用animate.vue

---
+++ src/components/animate.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <transition name="changemove">
+        </transition>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+<style type="text/css">
+
+</style>
